5aa98413f77dcced7340f231f42e05b97d131564
ashish-krishna-k <ashishk.krishna@gmail.com>
2023-08-07 14:06:55 +0530

    Sets-up a fresh project.

---
+++ bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+alert('hi!');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsS0FBSyxDQUFDLEtBQUssQ0FBQyxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImFsZXJ0KCdoaSEnKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
